db0297f21f14c0a9dec7d854f401000f31736517
isnit0 <joseph.reeve@googlemail.com>
2014-03-15 11:46:06 +0000

    Added Submit Event

--- developneur.js
+++ developneur.js
@@ -3,10 +3,19 @@ Forums = new Meteor.Collection('forums')
 Posts = new Meteor.Collection('posts')
 
 if (Meteor.isClient) {
+
+    /* Router Stuff */
     Meteor.Router.add({
 	'/':'home',
 	'/new':'newForum'
     });
+
+    /* New Forums*/
+    Template.newForum.events({
+	'submit':function(){
+	    console.log('hi');
+	}
+    })
 }
 
 if (Meteor.isServer) {
