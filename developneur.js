36e5b4cedb76a7b621efe6523762316c38e319b9
Iain Collins <me@iaincollins.com>
2014-03-15 15:21:58 +0100

    Bootstrappifying forums.

--- developneur.js
+++ developneur.js
@@ -6,48 +6,57 @@ if (Meteor.isClient) {
 
     /* Router Stuff */
     Meteor.Router.add({
-	'/':'home',
-	'/new':'newForum',
-	'/forums':'forums',
-	'/forum/:id':{to:'forum',and:function(id){
-	    Session.set('currentForumId', id)
-	}}
+        '/':'home',
+        '/new/forum':'newForum',
+        '/forums':'forums',
+        '/forum/:id':{to:'forum',and:function(id){
+            Session.set('currentForumId', id)
+        }}
     });
 
-    /* New Forum*/
+    Handlebars.registerHelper('loggedIn', function (id) {
+        if (Meteor.user()!=null) { return true } else { return false };
+    });
+    
+    Template.forums.events({
+        'click section':function(){
+            Meteor.Router.to('/forum/'+$(this)[0]._id);
+        }
+    });
+    
+    /* New Forum */
     Template.newForum.events({
-	'submit':function(){
-	    Forums.insert({siteId:1,name:$('#nameOfForum').val(),description:$('#descriptionOfForum').val()});
-	    console.log($('#nameOfForum').val());
-	}
+        'submit':function(){
+            Forums.insert({siteId:1,name:$('#nameOfForum').val(),description:$('#descriptionOfForum').val()});
+            console.log($('#nameOfForum').val());
+        }
     });
 
     /* Forums */
     Template.forums.allForums=function(){
-	return Forums.find();
+        return Forums.find();
     }
+    
     Template.forums.events({
-	'click section':function(){
-	    Meteor.Router.to('/forum/'+$(this)[0]._id);
-	}
+        'click section':function(){
+            Meteor.Router.to('/forum/'+$(this)[0]._id);
+        }
     });
 
     /* Forum */
     Template.forum.currentForum=function(){
-	return Forums.findOne(Session.get('currentForumId'))
+        return Forums.findOne(Session.get('currentForumId'))
     }
+    
     Template.forum.currentPosts=function(){
-	return Posts.find({forumId:Session.get('currentForumId')})
-    }
-    Template.forum.loggedIn=function(){
-	if(Meteor.user()!=null){return true}else{return false};
+        return Posts.find({forumId:Session.get('currentForumId')})
     }
     
     /* newPost */
     Template.newPost.events({
-	'submit':function(){
-	    Posts.insert({title:$('#newPostTitle').val(),details:$('#newPostDetails').val(),author:Meteor.user(),siteId:1,forumId:Session.get('currentForumId')});
-	}
+        'submit':function(){
+            Posts.insert({title:$('#newPostTitle').val(),details:$('#newPostDetails').val(),author:Meteor.user(),siteId:1,forumId:Session.get('currentForumId')});
+        }
     });
 
 }
